5422205fac56770ef4905c8c66f85f71292858e6
Pradeep Kumar <pradeep@keepworks.com>
2019-12-25 22:50:50 +0530

    Make PigeonMessagingService inherit directly from FirebaseMessagingService

--- android/src/main/java/io/pigeonapp/PigeonMessagingService.java
+++ android/src/main/java/io/pigeonapp/PigeonMessagingService.java
@@ -2,18 +2,27 @@ package io.pigeonapp;
 
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-public class PigeonMessagingService extends BaseMessagingService {
+public class PigeonMessagingService extends FirebaseMessagingService {
     private final String TAG = PigeonMessagingService.class.getSimpleName();
 
     @Override
-    public void onNewToken(String token) {
+    public void onNewToken(@NonNull String token) {
+        super.onNewToken(token);
+
         Log.d(TAG, "onNewToken: " + token);
+
+        PigeonClient.getInstance().setDeviceToken(token);
     }
 
     @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
         Log.d(TAG, "onMessageReceived: " + remoteMessage.getNotification().getTitle());
     }
 }
