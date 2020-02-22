e5380915e27c6582180257a8097cccbff32a70f7
Rajat Sharma <lunasunkaiser@gmail.com>
2020-02-22 18:52:55 +0530

    Fixed formatting

--- android/src/main/java/io/pigeonapp/PigeonLog.java
+++ android/src/main/java/io/pigeonapp/PigeonLog.java
@@ -3,48 +3,48 @@ package io.pigeonapp;
 import android.util.Log;
 
 public class PigeonLog {
-	private static enum LogLevel {
-		INFO(0), DEBUG(1), OFF(-1);
+		private static enum LogLevel {
+				INFO(0), DEBUG(1), OFF(-1);
 
-		private final int levelCode;
+				private final int levelCode;
 
-		LogLevel(int levelCode) {
-			this.levelCode = levelCode;
+				LogLevel(int levelCode) {
+						this.levelCode = levelCode;
+				}
+
+				public int getLevelCode() {
+				    return this.levelCode;
+				}
+
+				public static LogLevel lookupLogLevel(int levelCode) {
+						switch (levelCode) {
+								case 0:
+										return LogLevel.INFO;
+								case 1:
+										return LogLevel.DEBUG;
+								case -1:
+										return LogLevel.OFF;
+								default:
+										return LogLevel.INFO;
+						}
+				}
 		}
 
-		public int getLevelCode() {
-			return this.levelCode;
+		private static LogLevel logLevel = LogLevel.INFO;
+
+		public static void setLogLevel(int logLevel) {
+				PigeonLog.logLevel = LogLevel.lookupLogLevel(logLevel);
 		}
 
-		public static LogLevel lookupLogLevel(int levelCode) {
-			switch (levelCode) {
-				case 0:
-					return LogLevel.INFO;
-				case 1:
-					return LogLevel.DEBUG;
-				case -1:
-					return LogLevel.OFF;
-				default:
-					return LogLevel.INFO;
-			}
+		public static void d(String tag, String logText) {
+				if (logLevel.getLevelCode() > 0) {
+						Log.d(tag, logText);
+				}
 		}
-	}
 
-	private static LogLevel logLevel = LogLevel.INFO;
-
-	public static void setLogLevel(int logLevel) {
-		PigeonLog.logLevel = LogLevel.lookupLogLevel(logLevel);
-	}
-
-	public static void d(String tag, String logText) {
-		if (logLevel.getLevelCode() > 0) {
-			Log.d(tag, logText);
+		public static void i(String tag, String logText) {
+				if (logLevel.getLevelCode() == 0) {
+						Log.i(tag, logText);
+				}
 		}
-	}
-
-	public static void i(String tag, String logText) {
-		if (logLevel.getLevelCode() == 0) {
-			Log.i(tag, logText);
-		}
-	}
 }
