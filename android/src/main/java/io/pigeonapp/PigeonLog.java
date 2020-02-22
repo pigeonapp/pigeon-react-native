a225b4436769c46754ba797576aa2e7709a488f4
Rajat Sharma <lunasunkaiser@gmail.com>
2020-02-22 18:24:07 +0530

    Add enum for logLevels

--- android/src/main/java/io/pigeonapp/PigeonLog.java
+++ android/src/main/java/io/pigeonapp/PigeonLog.java
@@ -3,25 +3,48 @@ package io.pigeonapp;
 import android.util.Log;
 
 public class PigeonLog {
-    private static final int INFO = 0;
-    private static final int DEBUG = 1;
-    private static final int OFF = -1;
+	private static enum LogLevel {
+		INFO(0), DEBUG(1), OFF(-1);
 
-    private static int logLevel = PigeonLog.INFO;
+		private final int levelCode;
 
-    public static void setLogLevel(int logLevel){
-        PigeonLog.logLevel = logLevel;
-    }
+		LogLevel(int levelCode) {
+			this.levelCode = levelCode;
+		}
 
-    public static void d(String tag, String logText){
-        if(logLevel > 0){
-            Log.d(tag, logText);
-        }
-    }
+		public int getLevelCode() {
+			return this.levelCode;
+		}
 
-    public static void i(String tag, String logText){
-      if(logLevel == 0){
-          Log.i(tag, logText);
-      }
-  }
+		public static LogLevel lookupLogLevel(int levelCode) {
+			switch (levelCode) {
+				case 0:
+					return LogLevel.INFO;
+				case 1:
+					return LogLevel.DEBUG;
+				case -1:
+					return LogLevel.OFF;
+				default:
+					return LogLevel.INFO;
+			}
+		}
+	}
+
+	private static LogLevel logLevel = LogLevel.INFO;
+
+	public static void setLogLevel(int logLevel) {
+		PigeonLog.logLevel = LogLevel.lookupLogLevel(logLevel);
+	}
+
+	public static void d(String tag, String logText) {
+		if (logLevel.getLevelCode() > 0) {
+			Log.d(tag, logText);
+		}
+	}
+
+	public static void i(String tag, String logText) {
+		if (logLevel.getLevelCode() == 0) {
+			Log.i(tag, logText);
+		}
+	}
 }
