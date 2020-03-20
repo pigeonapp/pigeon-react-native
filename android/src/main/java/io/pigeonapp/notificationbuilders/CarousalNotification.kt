fc78624892f874bed0ebe4a22b24610ed5500b80
Rajat Sharma <lunasunkaiser@gmail.com>
2020-03-20 19:49:05 +0530

    Carousal Notification builder

--- android/src/main/java/io/pigeonapp/notificationbuilders/CarousalNotification.kt
+++ android/src/main/java/io/pigeonapp/notificationbuilders/CarousalNotification.kt
@@ -10,7 +10,6 @@ import android.os.Build
 import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.RemoteMessage
-import com.squareup.picasso.Picasso
 import io.pigeonapp.PigeonClient
 import io.pigeonapp.R
 import java.util.concurrent.atomic.AtomicInteger
@@ -22,22 +21,17 @@ fun showCarousal(notification: RemoteMessage.Notification) {
     val notifyId = id.incrementAndGet()
     val imgUrls = listOf("https://images.unsplash.com/photo-1548199973-03cce0bbc87b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80", "https://images.unsplash.com/photo-1444212477490-ca407925329e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1700&q=80", "https://images.unsplash.com/photo-1541876176131-3f5e84a7331a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1651&q=80")
 
-
     val applicationContext = PigeonClient.getInstance().reactApplicationContext.applicationContext as Application
     val packageName = applicationContext.packageName
     val currentAppIcon = applicationContext.packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA).icon
 
-
     val expandedView = RemoteViews(packageName, R.layout.notification_slider_layout)
     expandedView.setTextViewText(R.id.textViewTitle, notification.title)
 
+    val collapsedView = RemoteViews(packageName, R.layout.push_collapsed)
 
-    imgUrls.forEach {
-        val image = Picasso.get().load(it).get()
-        val viewFlipperImage = RemoteViews(packageName, R.layout.notification_slider_layout)
-        viewFlipperImage.setImageViewBitmap(R.id.imageView, image)
-        expandedView.addView(R.id.viewFlipper, viewFlipperImage)
-    }
+    collapsedView.setTextViewText(R.id.notificationTitle, notification.title)
+    collapsedView.setTextViewText(R.id.notificationText, notification.body)
 
     val notificationBuilder = NotificationCompat.Builder(applicationContext, channelId)
         .setSmallIcon(currentAppIcon)
